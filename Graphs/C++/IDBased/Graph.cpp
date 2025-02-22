490492f52d01de3c89925b832637b17b29b4bec5
tslime <taha.slime@gmail.com>
2025-02-22 00:53:57 -0500

    commit

--- Graphs/C++/IDBased/Graph.cpp
+++ Graphs/C++/IDBased/Graph.cpp
@@ -20,9 +20,10 @@ class Graph{
               
               int size;
               int num_vertices;
+              int degree;
               vector<Edge<T1,T2,T3>> e;
 
-              Graph(int size):size(size),num_vertices(0),e(size){}
+              Graph(int size):size(size),num_vertices(0),e(size),degree(0){}
 
               void printGraph(Graph<T1,T2,T3> gr){
 
@@ -44,9 +45,42 @@ class Graph{
                         }
                       }
                     }
-                   }
-                    
+                   }    
                }
+              }
+
+
+              void insertVertex(Graph<T1,T2,T3> gr,Vertex<T1,T2,T3> vx){
+                if(gr.num_vertices >= gr.size)
+                cout << "There are no spots where to insert a vertex \n";
+                else{
+                  if(gr.e[vx.id].v == nullptr){
+                    gr.e[vx.id].v = vx;
+                    gr.num_vertices++;
+                  }else cout << "This vertex position is alread taken";            
+                }
+              }
+
+              void insertEdge(Graph<T1,T2,T3> gr,T1 id1, T2 id2){
+                    if(gr.e[id1].v == nullptr || gr.e[id2].v == nullptr)
+                    cout << "One of the vertices does not exist \n";
+                    else{
+                      Vertex<T1,T2,T3> aux = gr.ed[id1].v;
+                      while(aux->next != nullptr)
+                      aux = aux->next;
+
+                      Vertex<T1,T2,T3> vx = new Vertex<T1,T2,T3>(gr.ed[2].v.id,gr.ed[2].v.label,gr.ed[2].v.data);
+                      aux->next = vx;
+                      gr.degree++;
+                    }
+              }
+
+              void deleteVertex(Graph<T1,T2,T3> gr,T1 id){
+
+              }
+
+               void deleteEdge(Graph<T1,T2,T3> gr,T1 id1, T2 id2){
+
               }
 
 };
