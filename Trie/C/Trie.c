44cea4c4dcb4fa357eb3dc83675c1516a7d4ec5e
tslime <taha.slime@gmail.com>
2025-03-20 01:52:47 -0400

    Trie data structure

--- Trie/C/Trie.c
+++ Trie/C/Trie.c
@@ -3,34 +3,54 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-//Forward declaration
-//struct TrieNode;
-//struct THashtable;
 
 
 //TrieNode data structure
-typedef struct TrieNode{
+typedef struct Trienode{
     bool endofw;
     struct THashtable *tn;
-}TrieNode;
+}Trienode;
 
 //Hashtable data structure
 typedef struct TNode{
     char *key;
-    TrieNode *child;
+    Trienode *child;
 }TNode;
 
 typedef struct THashtable{
     int size;
+    int num_c;
     TNode *children[];
 }THashtable;
 
+void initTNode();
 
+void resize();
 
 
 
 void main(){
 
+    int s; 
+    printf("Give me the size of your trie \n");
+    scanf("%d",&s);
+    printf("\n");
 
-    printf("test \n");
+    //initialization
+    Trienode *root = (Trienode*)(malloc(sizeof(Trienode))); 
+    root->tn = (THashtable*)(malloc(sizeof(THashtable) + s*sizeof(TNode)));
+    root->tn->size = s;
+    root->tn->num_c = 0;
+
+    int i;
+    for(i=0;i<s;i++){
+        root->tn->children[i] = (TNode*)(malloc(sizeof(TNode)));
+        root->tn->children[i]->key = NULL;
+        root->tn->children[i]->child = (Trienode*)(malloc(sizeof(Trienode)));
+        printf("test");
+    }
+
+    root->tn->children[0]->key = "test zero";
+    printf("test %d \n",root->tn->size);
+    printf("another test %s\n",root->tn->children[0]->key);
 }
