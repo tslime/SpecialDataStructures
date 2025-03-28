1eca19a37e585fa87c2d30df873e246fac80b61e
tslime <taha.slime@gmail.com>
2025-03-28 01:07:21 -0400

    Trie important algorithms completed

--- Trie/C/Trie.c
+++ Trie/C/Trie.c
@@ -2,7 +2,7 @@
 #include<malloc.h>
 #include<stdlib.h>
 #include<stdbool.h>
-
+#include<string.h>
 
 
 //TrieNode data structure
@@ -23,9 +23,132 @@ typedef struct THashtable{
     TNode *children[];
 }THashtable;
 
-void initTNode();
 
-void resize();
+//Function to initialize a trie node
+void initTNode(Trienode **nd,int s){
+    
+    *nd = (Trienode*)(malloc(sizeof(Trienode)));
+    (*nd)->tn = (THashtable*)(malloc(sizeof(THashtable) + s*sizeof(TNode)));
+    (*nd)->tn->size = s;
+    (*nd)->tn->num_c = 0;
+
+    int i;
+    for(i=0;i<s;i++){
+        (*nd)->tn->children[i] = (TNode*)(malloc(sizeof(TNode)));
+        (*nd)->tn->children[i]->key = NULL;
+        (*nd)->tn->children[i]->child = (Trienode*)(malloc(sizeof(Trienode)));
+        (*nd)->tn->children[i]->child = NULL;
+    }
+
+}
+
+//Function to resize a trie node hashtable 
+void resize(Trienode **nd,int new_size){
+
+    THashtable *t = (THashtable*)(malloc(sizeof(THashtable) + new_size*sizeof(TNode)));
+    t->size = new_size;
+
+    int i;
+    for(i=0;i<new_size;i++){
+
+        if( i < (*nd)->tn->size)
+        t->children[i] = (*nd)->tn->children[i];
+        else{
+            t->children[i] = (TNode*)(malloc(sizeof(TNode)));
+            t->children[i]->child = NULL;
+            t->children[i]->key = NULL;
+        }
+
+    }
+
+    t->num_c = (*nd)->tn->num_c; 
+    (*nd)->tn = t;
+}
+
+
+
+    
+//Insert word, iterative
+void insertString(Trienode *root,char *w){
+    
+    TNode *aux;
+
+    int i;
+    for(i=0;i<strlen(w);i++){
+        int index = w[i] - 'a';   
+
+        if(i == 0){
+            if(index >= root->tn->size)
+            resize(&root,index+1);
+
+            if(root->tn->children[index]->key == NULL){
+                root->tn->children[index]->key = &w[i];
+                root->tn->num_c++;
+            }
+            
+            aux = root->tn->children[index];
+
+        }else{
+            
+            if(aux->child == NULL){
+                initTNode(&(aux->child),index+1);
+                aux->child->tn->children[index]->key = &w[i];
+                aux->child->tn->num_c++;
+
+                aux = aux->child->tn->children[index];
+                
+            }else{
+                
+                if(index >= aux->child->tn->size){
+
+                    resize(&(aux->child),index+1);
+                    aux->child->tn->children[index]->key = &w[i];
+                    aux->child->tn->num_c++;
+                    
+                    aux = aux->child->tn->children[index];
+                }else{
+                    
+                    if(aux->child->tn->children[index]->key == NULL){
+                        aux->child->tn->children[index]->key = &w[i]; 
+                        aux->child->tn->num_c++;  
+                    }
+                    
+                    aux = aux->child->tn->children[index];
+                
+                }
+
+              
+            }
+        }   
+    }
+}
+
+void printTrie(Trienode *root,char *k,int ct){
+
+    if(root->tn->num_c != 0){
+        int i;
+        for(i = 0;i<root->tn->size;i++){
+            
+            if(root->tn->children[i]->key != NULL){
+                k[ct] = *(root->tn->children[i]->key);
+                printf("%s",k);
+                printf(" | ");
+
+                Trienode *aux = root->tn->children[i]->child;
+                if(aux != NULL){
+                     ct++;
+                     printTrie(aux,k,ct);
+                     k[ct] = '\0';
+                     ct--;
+
+                     if(ct == 0)
+                     printf("\n \n");
+                }
+            }  
+        }
+      }
+}
+
 
 
 
@@ -36,21 +159,77 @@ void main(){
     scanf("%d",&s);
     printf("\n");
 
+
     //initialization
-    Trienode *root = (Trienode*)(malloc(sizeof(Trienode))); 
-    root->tn = (THashtable*)(malloc(sizeof(THashtable) + s*sizeof(TNode)));
-    root->tn->size = s;
-    root->tn->num_c = 0;
+    Trienode *root;
+    initTNode(&root,s);
 
-    int i;
-    for(i=0;i<s;i++){
-        root->tn->children[i] = (TNode*)(malloc(sizeof(TNode)));
-        root->tn->children[i]->key = NULL;
-        root->tn->children[i]->child = (Trienode*)(malloc(sizeof(Trienode)));
-        printf("test");
-    }
 
-    root->tn->children[0]->key = "test zero";
-    printf("test %d \n",root->tn->size);
-    printf("another test %s\n",root->tn->children[0]->key);
+    
+    char *test1 =(char*)(malloc(100*sizeof(char)));
+    char *test2 =(char*)(malloc(100*sizeof(char)));
+    char *test3 =(char*)(malloc(100*sizeof(char)));
+    char *test4 =(char*)(malloc(100*sizeof(char)));
+    
+    printf("Give me a word \n");
+    scanf("%s",test1);
+    insertString(root,test1);
+    printf("\n");
+    printf("Give me another word \n");
+    scanf("%s",test2);
+    insertString(root,test2);
+    printf("\n");
+    printf("Give me one last word \n");
+    scanf("%s",test3);
+    insertString(root,test3);
+    printf("\n");
+    printf("Give me one more last word \n");
+    scanf("%s",test4);
+    insertString(root,test4);
+    //printf("super test\n");
+    
+
+    printf("\n");
+    char *c = (char*)malloc(100*sizeof(c));
+    printTrie(root,c,0);
+    printf("\n");
+
+   
+   
+   
+    /*
+    root->tn->children[2]->key = "c";
+    root->tn->num_c++;
+    
+
+    //printf("test 1 %s\n",root->tn->children[0]->key);
+    
+    
+    initTNode(&(root->tn->children[2]->child),s);
+    root->tn->children[2]->child->tn->children[0]->key = "a";
+    root->tn->children[2]->child->tn->num_c++;
+
+    
+
+
+    initTNode(&(root->tn->children[2]->child->tn->children[0]->child),s);
+
+   
+
+    root->tn->children[2]->child->tn->children[0]->child->tn->children[19]->key = "t";
+    root->tn->children[2]->child->tn->children[0]->child->tn->num_c++;
+    
+    
+    root->tn->children[2]->child->tn->children[0]->child->tn->children[17]->key = "r";
+    root->tn->children[2]->child->tn->children[0]->child->tn->num_c++;
+    printf("test\n");
+    //printf("test 2  %s\n",root->tn->children[0]->child->tn->children[0]->key);
+    */
+
+
+  
+  
+
+
+
 }
